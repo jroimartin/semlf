1b6d31015b9996e880283afdfeb6b3bed6f9917c
Roi Martin <jroi.martin@gmail.com>
2025-03-08 23:00:14 +0100

    semlf-tests.el, resources: simplify tests, add new test cases

--- semlf-tests.el
+++ semlf-tests.el
@@ -23,60 +23,46 @@
 (require 'ert-x)
 (require 'semlf)
 
-(ert-deftest semlf-fill-paragraph-function-test ()
+(ert-deftest semlf-test-fill-paragraph ()
+  "Fill paragraph."
+  (ert-test-erts-file (ert-resource-file "semlf-fill-paragraph.erts")
+                      (lambda ()
+			(semlf-fill-paragraph))))
+
+(ert-deftest semlf-test-fill-paragraph-function ()
   "Set `fill-paragraph-function' and fill paragraph."
   (ert-test-erts-file (ert-resource-file "semlf-fill-paragraph-function.erts")
                       (lambda ()
 			(setq-local fill-paragraph-function #'semlf-fill-paragraph)
 			(fill-paragraph))))
 
-(ert-deftest semlf-sentence-end-double-space-test ()
-  "Set `sentence-end-double-space'."
-  (ert-test-erts-file (ert-resource-file "semlf-sentence-end-double-space.erts")
-                      (lambda ()
-			(setq-local sentence-end-double-space t)
-			(semlf-fill-paragraph)
-			(forward-paragraph)
-			(setq-local sentence-end-double-space nil)
-			(semlf-fill-paragraph))))
-
-(ert-deftest semlf-fill-column-test ()
-  "Set `fill-column'."
-  (ert-test-erts-file (ert-resource-file "semlf-fill-column.erts")
-                      (lambda ()
-			(setq-local fill-column 70)
-			(semlf-fill-paragraph)
-			(forward-paragraph)
-			(setq-local fill-column 35)
-			(semlf-fill-paragraph))))
-
-(ert-deftest semlf-align-test ()
-  "Fill and align paragraph."
-  (ert-test-erts-file (ert-resource-file "semlf-align.erts")
+(ert-deftest semlf-test-justify ()
+  "Fill and justify paragraph."
+  (ert-test-erts-file (ert-resource-file "semlf-justify.erts")
                       (lambda ()
 			(semlf-fill-paragraph 'justify))))
 
-(ert-deftest semlf-second-paragraph-test ()
-  "Fill second paragraph."
-  (ert-test-erts-file (ert-resource-file "semlf-second-paragraph.erts")
+(ert-deftest semlf-test-sentence-end-double-space ()
+  "Set `sentence-end-double-space'."
+  (ert-test-erts-file (ert-resource-file "semlf-sentence-end-double-space.erts")
                       (lambda ()
-			(forward-paragraph)
+			(setq-local sentence-end-double-space nil)
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-last-paragraph-test ()
-  "Fill last paragraph."
-  (ert-test-erts-file (ert-resource-file "semlf-last-paragraph.erts")
+(ert-deftest semlf-test-fill-column ()
+  "Set `fill-column'."
+  (ert-test-erts-file (ert-resource-file "semlf-fill-column.erts")
                       (lambda ()
-			(goto-char (point-max))
+			(setq-local fill-column 35)
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-fill-prefix-test ()
-  "Insert fill prefix."
-  (ert-test-erts-file (ert-resource-file "semlf-fill-prefix.erts")
+(ert-deftest semlf-test-punctuation-marks ()
+  "Insert newline after period, exclamation mark and question mark."
+  (ert-test-erts-file (ert-resource-file "semlf-punctuation-marks.erts")
                       (lambda ()
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-fill-twice-test ()
+(ert-deftest semlf-test-fill-twice ()
   "Fill paragraph twice."
   (ert-test-erts-file (ert-resource-file "semlf-fill-twice.erts")
                       (lambda ()
@@ -85,31 +71,45 @@
 			(goto-char (point-min))
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-punctuation-marks-test ()
-  "Insert newline after period, exclamation mark and question mark."
-  (ert-test-erts-file (ert-resource-file "semlf-punctuation-marks.erts")
+(ert-deftest semlf-test-list ()
+  "Fill list."
+  (ert-test-erts-file (ert-resource-file "semlf-list.erts")
                       (lambda ()
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-emacs-lisp-mode-test ()
-  "Fill `emacs-lisp-mode' comment."
+(ert-deftest semlf-test-indented-block ()
+  "Fill indented block."
+  (ert-test-erts-file (ert-resource-file "semlf-indented-block.erts")
+                      (lambda ()
+			(semlf-fill-paragraph))))
+
+(ert-deftest semlf-test-emacs-lisp-mode ()
+  "Fill `emacs-lisp-mode' paragraph in comment."
   (ert-test-erts-file (ert-resource-file "semlf-emacs-lisp-mode.erts")
                       (lambda ()
 			(emacs-lisp-mode)
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-c-mode-test ()
-  "Fill `c-mode' comment."
+(ert-deftest semlf-test-c-mode ()
+  "Fill `c-mode' paragraph in comment."
   (ert-test-erts-file (ert-resource-file "semlf-c-mode.erts")
                       (lambda ()
 			(c-mode)
 			(semlf-fill-paragraph))))
 
-(ert-deftest semlf-org-mode-test ()
+(ert-deftest semlf-test-org-mode ()
   "Fill `org-mode' paragraph."
   (ert-test-erts-file (ert-resource-file "semlf-org-mode.erts")
                       (lambda ()
 			(org-mode)
+			(semlf-fill-paragraph))))
+
+(ert-deftest semlf-test-markdown-mode ()
+  "Fill `markdown-mode' paragraph."
+  (skip-unless (functionp 'markdown-mode))
+  (ert-test-erts-file (ert-resource-file "semlf-markdown-mode.erts")
+                      (lambda ()
+			(markdown-mode)
 			(semlf-fill-paragraph))))
 
 ;;; semlf-tests.el ends here
